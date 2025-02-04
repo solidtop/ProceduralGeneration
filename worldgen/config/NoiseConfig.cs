c7e2b3df73d49403f1eb2695705245530ad1c12c
solidtop <solidtop.asp@gmail.com>
2025-02-04 09:10:34 +0100

    Remove persistence and lacunarity as they probably won't be needed to config

--- worldgen/config/NoiseConfig.cs
+++ worldgen/config/NoiseConfig.cs
@@ -5,8 +5,6 @@
         public int Octaves { get; set; }
         public float Frequency { get; set; }
         public float Amplitude { get; set; }
-        public float Persistence { get; set; }
-        public float Lacunarity { get; set; }
         public float? Threshold { get; set; }
     }
 }
